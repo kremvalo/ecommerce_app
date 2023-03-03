58d32691f59fb498a9fd9a6f398444ede66cf5f0
Darwin Sáenz <darwinsaenz105@gmail.com>
2023-03-03 00:40:56 -0500

    updated cards and login

---
+++ Screens/VerifyPurchaseScreen/VerifyPurchaseScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function VerifyPurchaseScreen() {
+  return (
+    <View>
+      <Text>VerifyPurchaseScreen</Text>
+    </View>
+  );
+}
+
+export default VerifyPurchaseScreen;
